8957b9b4f807e0f717e18a71d34a2a67f2e0ea96
Qquehue <alejandro.qquehue@sptech.school>
2022-11-20 16:52:27 -0300

    entao toma

---
+++ tJava.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#///////////////////////CLONANDO APLICACAO///////////////////////////
+cd /home/ubuntu/Desktop
+mkdir Local.App
+git clone https://github.com/Qquehue/Console-Tech-Consulting.git
+cd Console-Tech-Consulting/API/local-app/target
+#///////////////////////////INICIANDO JAR////////////////////////////
+sudo su
+chmod 777 local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
+#///////////////////////////MISCELLANIUS/////////////////////////////
+xauth add $(xauth -f /home/ubuntu/.Xauthority list|tail -l)
+export DISPLAY=:0.0
+env |grep DISPLAY
+DISPLAY=:0.0
+
+classedodriver(com.mysql.cj.jdbc.Driver)
+Url: jdbc:mysql://localhost:3306/nomebanco?autoReconnect=true&useSSL=false
+username("root")
+password("urubu100")
