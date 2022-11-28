d8d1f5c5f4244b88f2ae310c57daeabfd883c375
Qquehue <alejandro.qquehue@sptech.school>
2022-11-28 14:08:09 -0300

    run docker

--- guiJava.sh
+++ guiJava.sh
@@ -12,5 +12,10 @@ cd /home/ubuntu/Desktop/Web.App/Console-Tech-Consulting/API/local-app/target
 java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
 }
 
+  sudo systemctl start docker
+  sudo systemctl enable docker
+sudo docker exec -it ctc /bin/bash 
+sudo docker run --name=ctc -d mysql/mysql-server:5.7
+
 sudo chmod 777 local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
 java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
